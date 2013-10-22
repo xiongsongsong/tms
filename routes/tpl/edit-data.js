9c94ab70e36702e7ab6b11f7b406146a0bd694f4
松松 <xiongsongsong@outlook.com>
2013-10-22 20:26:32 +0800

    明天开始制作form，并准备填充数据

---
+++ routes/tpl/edit-data.js
@@ -0,0 +1,31 @@
+/**
+ * Created by 松松 on 13-10-22.
+ */
+
+var app = require('app')
+var db = require('db')
+var helper = require('./helper')
+
+app.get(/\/edit\/data\/(.+)/, function (req, res) {
+    var tplSource = new db.Collection(db.Client, 'tpl-source')
+    var ObjectId = db.mongodb.ObjectID
+    try {
+        var id = ObjectId(req.params[0])
+    } catch (e) {
+        res.end('err:' + e)
+        return
+    }
+    var tplSource = new db.Collection(db.Client, 'tpl-source')
+    tplSource.findOne({page_id: id}, {sort: [
+        ['ts', -1]
+    ]}, function (err, docs) {
+        if (!err && docs) {
+
+            //开始获取模板的编辑数据
+            var result = helper.checkTemplate(docs.source)
+            res.render('tpl/edit-data', {docs: docs, result: JSON.stringify(result, undefined, '    ')})
+        } else {
+            res.end('404')
+        }
+    })
+})
--- routes/tpl/index.js
+++ routes/tpl/index.js
@@ -6,8 +6,12 @@
 var app = require('app')
 var db = require('db')
 
-//编辑模板源代码
-require('./edit')
-
 //新建模板
 require('./new')
+
+//编辑模板源代码
+require('./edit-source')
+
+//编辑模板数据
+require('./edit-data')
+
